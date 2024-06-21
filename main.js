48eab3cfe501157efe59504776a33ab5ffeb4b31
ArthurZorthea <130073812+ArthurZorthea@users.noreply.github.com>
2024-06-21 09:42:12 -0300

    21/06

--- main.js
+++ main.js
@@ -1,30 +1,45 @@
-const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".aba-conteudo");
-const tempo = document.querySelectorAll(".contador");
-tempo[0].textContent = "Bibs"
-const tempoObjetivo1 = new Date(2024,11,31,59,59);
-const tempoObjetivo2 = new Date(2024,11,31,59,59);
-const tempoObjetivo3 = new Date(2024,11,31,59,59);
-const tempoObjetivo4 = new Date(2024,11,31,59,59);
-const agora = new Date();
-let segundo;
-let minuto;
-let horas;
-let dias;
-segundos=(tempoObjetivo1-agora)/100
-minutos=segundo/60
-horas=minutos/60
-dias=horas/24
-tempo[1].textContent = agora
-tempo[0].textContent = tempoObjetivo1-agora
+{
+const botoes=document.querySelectorAll(".botao");
+const conteudo  = document.querySelectorAll(".aba-conteudo");
+const contador = document.querySelectorAll(".contador");
+const tempoObjetivo1= new Date(2027,5,9,17,49);
+const tempoObjetivo2= new Date(2027,10,18,14,29);
+const tempoObjetivo3= new Date(2025,3,9,10,30);
+const tempoObjetivo4= new Date(2024,10,9,17,20);
+let agora=new Date();
+ contador[0].textContent=tempoObjetivo1-agora
+ contador[1].textContent=tempoObjetivo2-agora
+ contador[2].textContent=tempoObjetivo3-agora
+ contador[3].textContent=tempoObjetivo4-agora
 
-for(let i = 0; i < botoes.length ; i++){
-    botoes[i].onclick = function(){
-        for (let j = 0 ; j < botoes.length ; j++){
-            botoes[j].classList.remove("ativo");
+ let segundos
+ let minutos
+ let horas
+ let dias
+
+  math.floor ()
+  segundos=(tempoObjetivo1-agora)/1000
+  minutos= segundos/60
+  horas= minutos/60
+  dias= horas/24
+  segundos = Math.floor (segundos);
+  minutos = Math.floor (minutos);
+  horas = Math.floor (horas);
+  dias = Math.floor (dias);
+  tempo[0].textContent = `faltam ${dias},${horas} horas, ${minutos} minutos e ${segundos} segundos`
+  tempo[1].textContent = agora  
+ 
+}
+
+
+for (let  i = 0 ; i < botoes.length ; i++){
+    botoes[i].onclick = function () {
+        for (let j = 0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo")
             conteudo[j].classList.remove("ativo")
         }
         botoes[i].classList.add("ativo")
         conteudo[i].classList.add("ativo")
+
     }
 }
